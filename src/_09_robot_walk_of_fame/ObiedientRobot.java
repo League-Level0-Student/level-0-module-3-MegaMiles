ca0e53d2967c56ff63c181f4c5d383009095e676
root <root@da36ecdf94b8>
2020-04-04 18:36:23 +0000

    ObiedientRobot

---
+++ src/_09_robot_walk_of_fame/ObiedientRobot.java
@@ -0,0 +1,70 @@
+package _09_robot_walk_of_fame;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class ObiedientRobot {
+ Robot jeff = new Robot();	
+public static void main(String[] args) {
+ObiedientRobot bob = new ObiedientRobot();  
+String shape =  JOptionPane.showInputDialog("what shape should the robot draw");	
+if(shape.equals()) {
+	
+}
+}
+void drawSquare(){
+	jeff.setSpeed(100);
+	jeff.setPenColor(new Color (50, 119, 168));
+	jeff.penDown();
+	jeff.move(200);	
+	jeff.turn(90);	
+	jeff.move(200);		
+	jeff.turn(90);	
+	jeff.move(200);	
+	jeff.turn(90);	
+	jeff.move(200);	
+	jeff.turn(90);
+
+
+
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
