22f1aaec627fa50c4e6e07cbf4114a2c51eaf289
root <root@cfabece18605>
2020-04-16 01:27:09 +0000

    hi

---
+++ src/Your_own_adventure.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class Your_own_adventure {
+public static void main(String[] args) {
+String fork = JOptionPane.showInputDialog("you are walking in the a dessreted area and then you hit a fork in the road, do you chose right or left?");
+if (fork.equals("right")){	
+String man = JOptionPane.showInputDialog("you continue walking and then you see a man do you run or fight him?");
+if (man.equals("fight")){
+JOptionPane.showMessageDialog(null,"End of your Adventure Try again(sad ending) ");
+
+}
+else {
+	JOptionPane.showInputDialog(null,"you barley escape,End of adventure Try again(happy ending) ");
+}
+}	
+if (fork.equals("left")) {
+JOptionPane.showMessageDialog(null,"you continue walking and then... BAM ");	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
